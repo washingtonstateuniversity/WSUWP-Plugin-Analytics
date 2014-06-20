72a14a78c0af73429b546e623d2493932470ab13
jeremyfelt <jeremy.felt@gmail.com>
2014-06-20 16:34:49 -0700

    Bump version to 0.0.4

--- wsu-analytics.php
+++ wsu-analytics.php
@@ -13,7 +13,7 @@ class WSU_Analytics {
 	/**
 	 * @var string The current version of this plugin, or used to break script cache.
 	 */
-	var $version = '0.0.3';
+	var $version = '0.0.4';
 
 	/**
 	 * Add our hooks.
