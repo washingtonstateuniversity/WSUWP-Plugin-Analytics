f1dab2da09afdaaea1d09b0c7aab4027ad4bb746
Jeremy Bass <jeremybass@cableone.net>
2015-02-25 11:23:52 -0800

    a few changes to cut the min size down, and only activate the admin parts if in the admin

--- js/analytics.js
+++ js/analytics.js
@@ -1,55 +1,56 @@
-(function($, window){
+(function($, window, analytics){
 	/* start with the defaults for WSU sites */
-	var rendered_accounts = []
+	var rendered_accounts = [];
 	
-	if(window.wsu_analytics.app.page_view_type=="Front End" || window.wsu_analytics.app.page_view_type=="unknown"){
+	if(analytics.app.page_view_type==="Front End" || analytics.app.page_view_type==="unknown"){
 		rendered_accounts = jQuery.merge( rendered_accounts , [{
-			id:window.wsu_analytics.wsuglobal.ga_code,
+			id:analytics.wsuglobal.ga_code,
 			settings:{
 				namedSpace:'WSUGlobal',
 				cookieDomain:".wsu.edu",
 				dimension:[
 					{'name':'dimension1','val': window.location.protocol },//protocol <string> (http: / https:)
-					{'name':'dimension2','val': window.wsu_analytics.wsuglobal.campus },//campus <string>
-					{'name':'dimension3','val': window.wsu_analytics.wsuglobal.college },//college <string>
-					{'name':'dimension4','val': window.wsu_analytics.wsuglobal.unit },//unit <string>
-					{'name':'dimension5','val': window.wsu_analytics.wsuglobal.subunit },//subunit <string>
-					{'name':'dimension6','val': window.wsu_analytics.app.is_authenticated }//editor <bool>
+					{'name':'dimension2','val': analytics.wsuglobal.campus },//campus <string>
+					{'name':'dimension3','val': analytics.wsuglobal.college },//college <string>
+					{'name':'dimension4','val': analytics.wsuglobal.unit },//unit <string>
+					{'name':'dimension5','val': analytics.wsuglobal.subunit },//subunit <string>
+					{'name':'dimension6','val': analytics.app.is_authenticated }//editor <bool>
 				],
-				events: window.wsu_analytics.wsuglobal.events
+				events: analytics.wsuglobal.events
 			}
 		}] );
 	}
 
 	rendered_accounts = jQuery.merge( rendered_accounts , [{
-		id: window.wsu_analytics.app.ga_code,
+		id: analytics.app.ga_code,
 		settings:{
 			namedSpace:'appScope',
 			cookieDomain:".wsu.edu",
 			dimension:[
-				{'name':'dimension1','val': window.wsu_analytics.app.page_view_type },//page_view_type <string>
-				{'name':'dimension2','val': window.wsu_analytics.app.authenticated_user },//authenticated_user <string>
+				{'name':'dimension1','val': analytics.app.page_view_type },//page_view_type <string>
+				{'name':'dimension2','val': analytics.app.authenticated_user },//authenticated_user <string>
 			],
-			events: window.wsu_analytics.app.events
+			events: analytics.app.events
 		}
 	}] );	
 
-	if(window.wsu_analytics.app.page_view_type=="Front End" && window.wsu_analytics.app.page_view_type=="unknown"){
-		if(window.wsu_analytics.site.ga_code!==false){
+	if(analytics.app.page_view_type==="Front End" || analytics.app.page_view_type==="unknown"){
+		if(analytics.site.ga_code!==false){
 			rendered_accounts = jQuery.merge( rendered_accounts , [{
-				id: window.wsu_analytics.site.ga_code,
+				id: analytics.site.ga_code,
 				settings:{
 					namedSpace:'siteScope',
 					cookieDomain:".wsu.edu",
 					dimension:[
-						{'name':'dimension1','val': window.wsu_analytics.app.is_authenticated }//editor <bool>
+						{'name':'dimension1','val': analytics.app.is_authenticated }//editor <bool>
 					],
-					events: window.wsu_analytics.site.events
+					events: analytics.site.events
 				}
 			}] );
 		}
 	}
-	console.log(rendered_accounts);
+	//console.log(analytics.app.page_view_type);
+	//console.log(rendered_accounts);
 	/* just so we are not tracking at this time
 	jQuery.jtrack({
 		analytics:{
@@ -57,4 +58,4 @@
 			accounts: rendered_accounts
 		}
 	});*/
-})(jQuery, window);
+})(jQuery, window, window.wsu_analytics);
